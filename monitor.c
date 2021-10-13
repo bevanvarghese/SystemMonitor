5296b7508668fc52d0530ab201a40888d622cc58
bevanvarghese <bevanvar@connect.hku.hk>
2021-10-13 19:49:26 +0800

    #2: requirements 4 & 5 without %s formatting

--- monitor.c
+++ monitor.c
@@ -5,10 +5,18 @@
 #include <sys/types.h>
 #include <sys/times.h>
 #include <time.h>
+#include <signal.h>
+#include <string.h>
 
 #define BILLION 1000000000L;
 
+void ignore_signal(int sig) {
+	signal(sig, SIG_IGN);
+	// printf("\nDeflated\n");
+}
+
 int main(int argc, char *argv[]) {
+	
 
 	if(argc==1) {
 		// terminate successfully without output
@@ -35,64 +43,49 @@ int main(int argc, char *argv[]) {
 
 			printf("exec: No such file or directory\n");
 			printf("\n");
-			// exit(-1);
 			return EXIT_FAILURE;
 
 		}
 		else {
 
-			// Parent Process
+			// parent process: monitor
 			printf("Parent Process Start.\n");
 
-			// clock_t starttime = clock();
-			struct timespec r_start, r_stop;
-			// clock_t u_start, u_stop;
+			// Let monitor ignore incoming interrupt-signals
+			// Will not work on SIGKILL and SIGSTOP
+			for(int i=1; i<32; i++) {
+				signal(i, ignore_signal);
+			}
 
-			// struct tms t;
-			// struct tms t1, t2;
-			// long tics_per_second = sysconf(_SC_CLK_TCK);
-			// printf("%d\ntics\n", tics_per_second);
+			struct timespec r_start, r_stop;
 
 			int status;
 			clock_gettime(CLOCK_MONOTONIC, &r_start);
-			// u_start = clock();
-			// if ((u_start = times (&t1)) == -1)     
-      //   perror ("times");
 			waitpid(pid, &status, 0);
 			clock_gettime(CLOCK_MONOTONIC, &r_stop);
-			// u_stop = clock();
-			// if ((u_stop = times (&t2)) == -1)     
-      //   perror ("times");
 
+			// cannot be started 
 			if(WIFEXITED(status)) {
-				int es = WEXITSTATUS(status);
-				// printf("es%d\n", es);
-				if(es==EXIT_FAILURE) {
+				// printf("WEXITSTATUS(status) %d\n", WEXITSTATUS(status));
+				if(WEXITSTATUS(status)==EXIT_FAILURE) {
 					printf("monitor experienced an error in starting the command: %s\n", argv[current_proc_idx]);
 				}
+			}
+			// signalled
+			// TODO: format the string
+			if(WIFSIGNALED(status)) {
+				printf("The command \"%s\" is interrupted by the signal number = %d (%s)\n\n", argv[current_proc_idx], WTERMSIG(status), strsignal(WTERMSIG(status)));
 			}
 			
 			// http://www.qnx.com/developers/docs/6.5.0/index.jsp?topic=%2Fcom.qnx.doc.neutrino_lib_ref%2Fc%2Fclock_gettime.html
 			double realtime = (r_stop.tv_sec-r_start.tv_sec) + (double)(r_stop.tv_nsec - r_start.tv_nsec) / (double)BILLION;
-			// double usertime = ((double) u_stop - u_start)/CLOCKS_PER_SEC;
-			// printf("%f\n", (double) u_stop);
-			// printf("%f\n", (double) u_start);
-			// double usertime = (double) (t.tms_cutime)/tics_per_second;
-			// double systime = (double) (t.tms_cstime)/tics_per_second;
-			// printf ("u_start = %ld, times: %ld %ld %ld %ld\n", u_start, t1.tms_utime,  
-      //   t1.tms_cutime, t1.tms_stime, t1.tms_cstime);     
-    	// printf ("u_stop = %ld, times: %ld %ld %ld %ld\n", u_stop, t2.tms_utime,     
-      //   t2.tms_cutime, t2.tms_stime, t2.tms_cstime);     
-    	// printf ("u_stop - u_start = %ld\n", u_stop - u_start); 
-			// printf ("t2.user - t1.user = %ld\n", t2.tms_cutime - t1.tms_cutime); 
-			// printf ("t2.sys - t1.sys = %ld\n", t2.tms_cstime - t1.tms_cutime);
-			// printf ("real: %.3f s\n", realtime);
 
 			// printf("real: %.3f s, user: %.3f s, system: %.3f s\n", realtime, usertime, systime);
 			printf("\nreal: %.3f s, user: %.3f s, system: %.3f s\n", realtime, 0.0, 0.0);
 			
 			printf("Parent Process End.\n");
 			printf("\n");
+
 		}
 
 	} 
